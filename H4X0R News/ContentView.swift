6231053dbf55668ad1f0bf8c7add2442797e2a55
Naidu <panaidu541@gmail.com>
2020-02-18 13:13:18 +0530

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Appala Naidu on 18/02/20.
+//  Copyright © 2020 Appala Naidu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
